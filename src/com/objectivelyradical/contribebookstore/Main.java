87a63bb1d413c1e80397e32569bacb909677e009
Corey <c.mark@creativehope.co.jp>
2016-09-08 20:11:48 +0900

    Finished the console interface

--- src/com/objectivelyradical/contribebookstore/Main.java
+++ src/com/objectivelyradical/contribebookstore/Main.java
@@ -2,12 +2,14 @@ package com.objectivelyradical.contribebookstore;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 	final static String DEFAULT_BOOK_URL = "http://www.contribe.se/bookstoredata/bookstoredata.txt";
 	static BookStore bookStore;
-	static ArrayList<Book> cart = new ArrayList<Book>();
+	static HashMap<Book, Integer> cart = new HashMap<Book, Integer>();
 	static Scanner s;
 	
 	// The main class, which operates as a rudimentary front-end. It interacts only with the middleware-like BookStore class.
@@ -26,11 +28,13 @@ public class Main {
 				searchScreen();
 				break;
 			case(2):
+				addBookScreen();
 				break;
 			case(3):
 				cartScreen();
 				break;
 			case(4):
+				checkout();
 				break;
 			case(5):
 				System.exit(0);
@@ -49,6 +53,7 @@ public class Main {
 		clear();
 		System.out.println("+++++++++++++++++++++++");
 		System.out.println("+ CONTRIBE BOOK STORE +");
+		System.out.println("+      Main Menu      +");
 		System.out.println("+++++++++++++++++++++++");
 		System.out.println("");
 		System.out.println("");
@@ -136,12 +141,16 @@ public class Main {
 			System.out.println("Author: " + book.getAuthor());
 			System.out.println("Price: " + book.getPrice());
 			System.out.println("");
-			System.out.println("Buy this book? (Y/N)");
+			System.out.println("Add this book to your cart? (Y/N)");
 			
 			String next = s.next().trim().toUpperCase();
 			if(next.equals("Y")) {
 				// Put the book in our cart and then return to the results
-				cart.add(book);
+				if(cart.containsKey(book)) {
+					cart.put(book, cart.get(book) + 1);
+				} else {
+					cart.put(book, 1);
+				}
 				return;
 			} else if(next.equals("N")) {
 				// Return to the search results here
@@ -159,24 +168,219 @@ public class Main {
 			System.out.println("");
 			Book b;
 			BigDecimal total = new BigDecimal(0);
-			for(int i = 0; i < cart.size(); i++) {
-				b = cart.get(i);
-				System.out.println((i + 1) + ") " + b.getTitle() + ", " + b.getAuthor() + ", " + b.getPrice());
-				total = total.add(b.getPrice());
+			int i = 0;
+			if(cart.keySet().size() == 0) {
+				System.out.println("");
+				System.out.println("No items in your cart (yet).");
+			} else {
+				for (Book key : cart.keySet()) {
+					b = key;
+					System.out.println((i + 1) + ") " + b.getTitle() + ", " + b.getAuthor() + ", " + b.getPrice() + "     x" + cart.get(b));
+					total = total.add((b.getPrice().multiply(new BigDecimal(cart.get(b)))));
+					
+					i++;
+				}
+				System.out.println("");
+				System.out.println("Total: " + total);
+				System.out.println("");
 			}
-			System.out.println("");
-			System.out.println("Total: " + total);
-			System.out.println("");
+			
 			System.out.println("E) Empty cart, <) Back to main menu");
 			
 			String next = s.next().trim().toUpperCase();
 			if(next.equals("E")) {
 				// Clear the cart, and head back to the main menu
-				cart = new ArrayList<Book>();
+				cart = new HashMap<Book, Integer>();
 				return;
 			} else if(next.equals("<")) {
 				// Just go back to the main menu
 				return;
+			}
+		}
+	}
+	
+	private static void checkout() {
+		clear();
+		s = new Scanner(System.in);
+		
+		// Prep the array we're going to purchase
+		ArrayList<Book> bookList = new ArrayList<Book>();
+		for(Book key : cart.keySet()) {
+			for(int i = 0; i < cart.get(key); i++) {
+				bookList.add(key);				
+			}
+		}
+		Book[] books = bookList.toArray(new Book[bookList.size()]);
+		int length = bookList.size();
+		if(length > 0) {
+			int[] results = bookStore.buy(books);
+			cart.clear();
+			int errorCount = 0;
+			for(int i = 0; i < length; i++) {
+				if(results[i] == PurchaseStatus.OK) {
+					continue;
+				} else {
+					errorCount++;
+					clear();
+					String error;
+					if(results[i] == PurchaseStatus.DOES_NOT_EXIST) {
+						error = "Unable to find book: " + books[i].getTitle();
+					} else if(results[i] == PurchaseStatus.NOT_IN_STOCK) {
+						error = "The following book is out of stock: " + books[i].getTitle();
+					} else {
+						error = "An unforseen error has occurred. Please contact the programmer for a full refund.";
+					}
+					System.out.println("++++++++++++++++++++++++++");
+					System.out.println("+        CHECKOUT        +");
+					System.out.println("++++++++++++++++++++++++++");
+					System.out.println("");
+					System.out.println(error);
+					System.out.println("");
+					System.out.println("Press ENTER to continue");
+					s = new Scanner(System.in);
+					s.nextLine();
+				}				
+				System.out.println("");
+			}
+			if(errorCount < length) {
+
+				clear();
+				System.out.println("++++++++++++++++++++++++++");
+				System.out.println("+        CHECKOUT        +");
+				System.out.println("++++++++++++++++++++++++++");
+				System.out.println("");
+				System.out.println("Successfully purchased " + (length - errorCount) + " of " + length + " books.");
+				System.out.println("Please have a nice day.");
+				System.out.println("");
+				System.out.println("Press ENTER to return to the main menu");
+				if(s.hasNextLine()) {
+					return;
+				}
+			}
+			return;
+		} else {
+
+			clear();
+			System.out.println("++++++++++++++++++++++++++");
+			System.out.println("+        CHECKOUT        +");
+			System.out.println("++++++++++++++++++++++++++");
+			System.out.println("");
+			System.out.println("Your cart is currently empty. Please add some books before checkout out.");
+			System.out.println("");
+			System.out.println("Press ENTER to return to the main menu");
+			if(s.hasNextLine()) {
+				return;
+			}
+		}
+	}
+	
+	
+	
+	// This is the worst method I've ever written.
+	private static void addBookScreen() {
+		while(true) {
+			String title = "";
+			String author = "";
+			BigDecimal price;
+			int quantity = 0;
+			
+			clear();
+			System.out.println("++++++++++++++++++++++++");
+			System.out.println("+     ADD NEW BOOK     +");
+			System.out.println("++++++++++++++++++++++++");
+			System.out.println("");
+			System.out.println("Enter book title, or \"<\" to return to the main menu.");
+			System.out.println("");
+			System.out.print("Title: ");
+			s = new Scanner(System.in);
+			String next = s.nextLine().trim();
+			if(next.equals("<")) {
+				// Back to the main menu
+				return;
+			} else {
+				title = next;
+				while(true) {
+					clear();
+					System.out.println("++++++++++++++++++++++++");
+					System.out.println("+     ADD NEW BOOK     +");
+					System.out.println("++++++++++++++++++++++++");
+					System.out.println("");
+					System.out.println("Enter book author, \",\" to go back, or \"<\" to return to the main menu.");
+					System.out.println("");
+					System.out.print("Author: ");
+					next = s.nextLine().trim();
+					if(next.equals("<")) {
+						// Back to the main menu
+						return;
+					} else if(next.toUpperCase().equals(",")){
+						break;
+					} else {
+						author = next;
+						while(true) {
+							clear();
+							System.out.println("++++++++++++++++++++++++");
+							System.out.println("+     ADD NEW BOOK     +");
+							System.out.println("++++++++++++++++++++++++");
+							System.out.println("");
+							System.out.println("Enter book price, \",\" to go back, or \"<\" to return to the main menu.");
+							System.out.println("");
+							System.out.print("Price: ");
+							boolean isBigDecimal = s.hasNextBigDecimal();
+							next = s.nextLine().trim();
+							if(next.equals("<")) {
+								// Back to the main menu
+								return;
+							} else if(next.equals(",")){
+								break;
+							} else if(isBigDecimal){
+								price = new BigDecimal(next);
+								while(true) {
+									clear();
+									System.out.println("++++++++++++++++++++++++");
+									System.out.println("+     ADD NEW BOOK     +");
+									System.out.println("++++++++++++++++++++++++");
+									System.out.println("");
+									System.out.println("Enter quantity, \",\" to go back, or \"<\" to return to the main menu.");
+									System.out.println("");
+									System.out.print("Quantity: ");
+									boolean isInt = s.hasNextInt();
+									next = s.nextLine().trim();
+									if(next.equals("<")) {
+										// Back to the main menu
+										return;
+									} else if(next.toUpperCase().equals(",")){
+										break;
+									} else if(isInt){
+										quantity = Integer.parseInt(next);
+										while(true) {
+											clear();
+											System.out.println("++++++++++++++++++++++++");
+											System.out.println("+     ADD NEW BOOK     +");
+											System.out.println("++++++++++++++++++++++++");
+											System.out.println("");
+											System.out.println("Title: " + title);
+											System.out.println("Author: " + author);
+											System.out.println("Price: " + price);
+											System.out.println("Quantity: " + quantity);
+											System.out.println("");
+											System.out.println("Add this book (Y/N)? Or enter \",\" to go back, or \"<\" to return to the main menu.");
+											System.out.println("");
+											next = s.nextLine().trim().toUpperCase();
+											if(next.equals("<") || next.toUpperCase().equals("N")) {
+												return;
+											} else if (next.toUpperCase().equals(",")){
+												break;
+											} else if (next.toUpperCase().equals("Y")) {
+												bookStore.addNewBook(title,  author,  price.toString(), quantity);
+												return;
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
 			}
 		}
 	}
